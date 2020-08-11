6858ea7ed46c1a5f75a8f3a364859d51aa51437d
Alexander Buechel <abuech2s@gmail.com>
2020-08-11 19:04:32 +0200

    refactoring of config var names

--- src/main/java/sim/config/Constants.java
+++ src/main/java/sim/config/Constants.java
@@ -2,15 +2,19 @@ package sim.config;
 
 public class Constants {
 
+	//Identifiers
 	public static final String TOKEN_ADSB = "adsb";
 	public static final String TOKEN_AIS = "ais";
 	public static final String TOKEN_GPS = "gps";
 	public static final String TOKEN_RADAR = "radar";
 	
-	public static final String configFileName = "config.xml";
-	public static final int configReloadTime = 15_000;
+	//Config parameters
+	public static final String CONFIG_FILENAME = "config.xml";
+	public static final int CONFIG_RELOADTIME = 15_000;
 	
+	//Track parameters
 	public static final long TRACK_SLEEP_TIME = 600 * 1_000L; // in [s]
 	
+	//Physical constants
 	public static final double fromMtoNM = 0.000539957; // from [m] to [NM]
 }
