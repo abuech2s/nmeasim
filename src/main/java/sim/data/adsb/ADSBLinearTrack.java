53ddb031354fc14ae7ace8c4759a03cb55873435
alexander.buechel <alexander.buechel@schoenhofer.de>
2020-11-16 13:01:19 +0100

    fix too large amount of adsb data

--- src/main/java/sim/data/adsb/ADSBLinearTrack.java
+++ src/main/java/sim/data/adsb/ADSBLinearTrack.java
@@ -29,7 +29,7 @@ public class ADSBLinearTrack extends Track {
 	private int position = 0;
 	
 	public ADSBLinearTrack(Config config, String hexIdent, String callsign) {
-		super(config, 250.0, 1.0);
+		super(config, 250.0, 3.0);
 		this.hexIdent = hexIdent;
 		this.callsign = callsign;
 		init();
