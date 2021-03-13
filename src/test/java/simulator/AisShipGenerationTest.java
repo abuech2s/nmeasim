103a246682ab5cab0c09a8a487e5d49a0bec29be
Alexander Buechel <abuech2s@gmail.com>
2021-03-13 09:08:11 +0100

    add shipGenerationTest

---
+++ src/test/java/simulator/AisShipGenerationTest.java
@@ -0,0 +1,35 @@
+package simulator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import sim.data.ais.data.ship.IShip;
+import sim.data.ais.data.ship.ShipFactory;
+
+public class AisShipGenerationTest {
+
+	@Test
+	public void AisShipGenerationTest1() {
+		
+		for (int i = 0; i < 10; i++) {
+			IShip ship = ShipFactory.getRandomShip();
+			
+			assertTrue(ship.getCallsign() != null);
+			assertTrue(!ship.getCallsign().isEmpty());
+			
+			assertTrue(ship.getVesselName() != null);
+			assertTrue(!ship.getVesselName().isEmpty());
+			
+			assertTrue(ship.getBow() > 0);
+			assertTrue(ship.getDraught() > 0);
+			assertTrue(ship.getPort() > 0);
+			assertTrue(ship.getStarboard() > 0);
+			assertTrue(ship.getStern() > 0);
+			
+			assertTrue(ship.getImo() > 0);
+			assertTrue(ship.getShipType() > 0);
+		}
+	}
+	
+}
